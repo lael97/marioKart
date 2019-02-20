19474d57e8de371d17e1b147f2c44ea90ce0736b
Alexis Gonzalez <agonzalezgarcia@csumb.edu>
2019-02-20 14:09:25 -0800

    Initial Commit

---
+++ marioKart/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  marioKart
+//
+//  Created by Alexis Gonzalez on 2/20/19.
+//  Copyright © 2019 alexis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
